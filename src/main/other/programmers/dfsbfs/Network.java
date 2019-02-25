c146ba3e5c53fb3e19d150abe064d996533f88bc
sehun <18029569+Sehun-Kim@users.noreply.github.com>
2019-02-25 23:59:38 +0900

    programmers dfs 2

--- src/main/other/programmers/dfsbfs/Network.java
+++ src/main/other/programmers/dfsbfs/Network.java
@@ -1,14 +1,84 @@
 package main.other.programmers.dfsbfs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Network {
     public static void main(String[] args) {
-        int n = 3;
-        int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        int[][] computers = {{1, 0, 1}, {0, 1, 1}, {1, 1, 1}};
+        int n = computers.length;
         int result = solution(n, computers);
         System.out.println(result);
     }
 
-    private static int solution(int n, int[][] computers) {
-        return 0;
+    static class Computer {
+        boolean visit;
+        int ip;
+        List<Integer> linkedIp;
+
+        public Computer(int ip) {
+            this.ip = ip;
+            this.visit = false;
+            this.linkedIp = new ArrayList<>();
+        }
+
+        void settingLink(int[] ips) {
+            for (int i = 0; i < ips.length; i++) {
+                if (ips[i] == 1 && i != ip) {
+                    this.linkedIp.add(i);
+                }
+            }
+        }
+
+        public boolean isVisit() {
+            return this.visit;
+        }
+
+        public void visitNetwork(List<Computer> computerList) {
+            for (Integer integer : linkedIp) {
+                Computer com = computerList.get(integer);
+                if (!com.isVisit()) {
+                    com.visiting().visitNetwork(computerList);
+                }
+            }
+        }
+
+        private Computer visiting() {
+            this.visit = true;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "Computer{" +
+                    "visit=" + visit +
+                    ", ip=" + ip +
+                    ", linkedIp=" + linkedIp +
+                    '}';
+        }
     }
+
+    public static int solution(int n, int[][] computers) {
+        List<Computer> computerList = new ArrayList<>();
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            Computer com = new Computer(i);
+            com.settingLink(computers[i]);
+            computerList.add(com);
+            System.out.println(com);
+        }
+
+        for (Computer computer : computerList) {
+            if (!computer.isVisit()) {
+                computer.visitNetwork(computerList);
+                computer.visiting();
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+
 }
